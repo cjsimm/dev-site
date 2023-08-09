b60ac8f57e3abc910c3e38ce9668d6c7f96057de
gb <glumbongo93@gmail.com>
2023-08-09 12:17:22 +0100

    improved contact form and added styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,25 +11,32 @@ export default function contact() {
     return (
       <main>
         <section className={`sectionFillPage flexCentered bgRed`}>
+            
             <h1>Contact</h1>
-            <p> 
-                For any business enquiries, questions, or comments, 
-                please get in touch using the contact form below:
-            </p>
-            <form className={styles.contactForm} id="contact">
-                <div className={styles.cfContainer}>
-                  <label htmlFor="name">Name:</label>
-                  <input className={styles.cfName} name="name" type="text" form='contact' required/>
-                </div>
-                <div className={styles.cfContainer}>
-                  <label htmlFor="message">Message:</label>
-                  <input className={styles.cfMessage} name="message" type="text" form='contact' required/>
-                </div>
-                <div className={styles.cfContainer}>
-                  <label htmlFor='submit'></label>
-                  <input className={styles.cfSubmit} name="submit" type="submit" form='contact'/>
-                </div>
-            </form>
+            <div className="sectionContent">
+              <p> 
+                  For any business enquiries, questions, or comments, 
+                  please get in touch using the contact form below:
+              </p>
+              <form className={styles.contactForm} id="contact">
+                  <div className={styles.cfContainer}>
+                    <label htmlFor="name">Email Address*:</label>
+                    <input className={styles.cfName} name="name" type="text" form='contact' required/>
+                  </div>
+                  <div className={styles.cfContainer}>
+                    <label htmlFor="subject">Subject*:</label>
+                    <input className={styles.cfSubject} name="subject" type="text" form='contact' required/>
+                  </div>
+                  <div className={styles.cfContainer}>
+                    <label htmlFor="message">Message*:</label>
+                    <textarea className={styles.cfMessage} name="message" form='contact' required/>
+                  </div>
+                  <div className={styles.cfContainer}>
+                    <label htmlFor='submit'></label>
+                    <input className={styles.cfSubmit} name="submit" type="submit" form='contact'/>
+                  </div>
+              </form>
+            </div>
         </section>
       </main>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from '@/node_modules/next/link';
 import type { Metadata } from 'next';
 
 const page = 'Home';
@@ -14,14 +15,16 @@ export default function Home() {
     <main>
       <section className={`sectionBlock flexCentered bgGreen`}>
         <h1>Hello.</h1>
-        <p>This is placeholder text to test the section styles</p>
+        <div className="sectionContent">
+          <p>This is placeholder text to test the section styles</p>
+        </div>
       </section>
       <section className={`sectionBlock flexCentered bgGrey`}>
         <h1>Hello.</h1>
-        <p>This is placeholder text to test the section styles</p>
+        <p>Available in <Link href="/cv/static">PDF Format</Link></p>
       </section>
       <section className={`sectionBlock flexCentered bgBlue`}>
-        <h1>Hello.</h1>
+        <h1>Latest Blog Posts</h1>
         <p>This is placeholder text to test the section styles</p>
       </section>
     </main>
