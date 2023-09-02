955deb55d213037eb6860165a524f044bb1b04ad
gb <glumbongo93@gmail.com>
2023-09-02 15:44:48 +0100

    fleshed out portfolio items and added mock data

--- app/(portfolio)/_components/PortfolioItem.tsx
+++ app/(portfolio)/_components/PortfolioItem.tsx
@@ -1,29 +1,30 @@
-"use client"; //tells nextjs to make client component (holds state)
 import { useState } from "react";
 import styles from './components.module.css';
 import Image from "next/image";
+import { portfolioItemData } from "./PortfolioMenu";
 
 //function that holds the portfolio item description details and preview
 //can transition between a minimized and expanded view 
-// state held: isExpanded (opens and closes the expanded view)
-export default function PortfolioItem(): JSX.Element {
-    const [expanded, setExpanded] = useState(false);
+export default function PortfolioItem({data}: {data: portfolioItemData}): JSX.Element {
     return (
         <div className={styles.portfolioItemContainer}>
-            {expanded ? (
+            {data.isExpanded ? (
                 <div className={styles.portfolioItemExpanded}>
                     <p>{"This portfolio item is expanded!"}</p>
                 </div>
             ) : (
-                <div className={styles.portfolioItemSmall}>
+                <button className={styles.portfolioItemSmall}>
                     <Image
                         src="/logo.svg"
                         alt="placeholder image"
-                        width={32}
-                        height={32}
+                        width={200}
+                        height={200}
                     />
-                    <p>{"Hello World!"}</p>
-                </div>
+                    <div className={styles.itemSmallText}>
+                        <h1>{data.title}</h1>
+                        <p>{data.description}</p>
+                    </div>
+                </button>
             )}
         </div>
     );
--- app/(portfolio)/_components/PortfolioMenu.tsx
+++ app/(portfolio)/_components/PortfolioMenu.tsx
@@ -1,15 +1,52 @@
-import { ReactComponentElement } from "react";
+"use client";
+import { useState } from "react";
 import PortfolioItem from "./PortfolioItem";
 import styles from './components.module.css';
 
 //holds the list of portfolio items
+//holds state of which portfolio items are expanded 
+export type portfolioItemData = {
+    isExpanded: boolean
+    title: string,
+    thumbnail: string,
+    description: string
+}
+
+
 export default function PortfolioMenu(): JSX.Element {
 
+    const data: portfolioItemData[] = [
+        {   
+            isExpanded: false,
+            title: "Hello", 
+            thumbnail: "placeholder", 
+            description: "this is a placeholder description"
+        },
+        {   
+            isExpanded: false,
+            title: "Hello 2", 
+            thumbnail: "placeholder", 
+            description: "this is a placeholder description"
+        },
+        {   
+            isExpanded: false,
+            title: "Hello 3", 
+            thumbnail: "placeholder", 
+            description: "this is a placeholder description"
+        },
+        {   
+            isExpanded: false,
+            title: "Hello 4", 
+            thumbnail: "placeholder", 
+            description: "this is a placeholder description"
+        }
+    ]
     return (
         <div className={styles.portfolioMenuContainer}>
-            <PortfolioItem />
-            <PortfolioItem />
-            <PortfolioItem />
+            <PortfolioItem data={data[0]} />
+            <PortfolioItem data={data[1]}/>
+            <PortfolioItem data={data[2]}/>
+            <PortfolioItem data={data[3]}/>
         </div>
     );
 }
