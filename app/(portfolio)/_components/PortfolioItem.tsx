3fbf72fe6028c2ad44aaa2c8397f4318c0961cc3
gb <glumbongo93@gmail.com>
2023-09-03 14:40:31 +0100

    state change working. need to refactor

--- app/(portfolio)/_components/PortfolioItem.tsx
+++ app/(portfolio)/_components/PortfolioItem.tsx
@@ -5,15 +5,25 @@ import { portfolioItemData } from "./PortfolioMenu";
 
 //function that holds the portfolio item description details and preview
 //can transition between a minimized and expanded view 
-export default function PortfolioItem({data}: {data: portfolioItemData}): JSX.Element {
+export default function PortfolioItem(
+    {data, itemID, onExpand}: {
+    data: portfolioItemData, 
+    itemID: number,
+    onExpand: (itemID: number) => void,
+}): JSX.Element {
+
+    const handleClick = () => {
+        onExpand(itemID);
+    }
+
     return (
         <div className={styles.portfolioItemContainer}>
             {data.isExpanded ? (
-                <div className={styles.portfolioItemExpanded}>
+                <button className={styles.portfolioItemExpanded} onClick={handleClick}>
                     <p>{"This portfolio item is expanded!"}</p>
-                </div>
+                </button>
             ) : (
-                <button className={styles.portfolioItemSmall}>
+                <button className={styles.portfolioItemSmall} onClick={handleClick}>
                     <Image
                         src="/logo.svg"
                         alt="placeholder image"
--- app/(portfolio)/_components/PortfolioMenu.tsx
+++ app/(portfolio)/_components/PortfolioMenu.tsx
@@ -1,7 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PortfolioItem from "./PortfolioItem";
 import styles from './components.module.css';
+import { JsxElement } from "typescript";
+
+const data: portfolioItemData[] = [
+    {   
+
+        isExpanded: false,
+        title: "Hello", 
+        thumbnail: "placeholder", 
+        description: "this is a placeholder description"
+    },
+    {   
+
+        isExpanded: false,
+        title: "Hello 2", 
+        thumbnail: "placeholder", 
+        description: "this is a placeholder description"
+    },
+    {   
+
+        isExpanded: false,
+        title: "Hello 3", 
+        thumbnail: "placeholder", 
+        description: "this is a placeholder description"
+    },
+    {   
+
+        isExpanded: false,
+        title: "Hello 4", 
+        thumbnail: "placeholder", 
+        description: "this is a placeholder description"
+    }
+]
+
 
 //holds the list of portfolio items
 //holds state of which portfolio items are expanded 
@@ -12,41 +45,74 @@ export type portfolioItemData = {
     description: string
 }
 
+//redesign
+// if the clicked item matches the expanded item id, handle expandsets the state to  undefined  . in use effect, if the state is undefined, we draw the default set of portfolio items 
+// if the clicked item doesnt match the expanded item id, handle expandd sets the state of that new item id. in use effect, we draw the portfolio item thumbnails for all except the state and we draw that one as the expanded view
+
+// if state undefined, draw portfolio item standards
+// if state 
+    // if state == id draw big component
+    // else draw thumbnail component 
 
 export default function PortfolioMenu(): JSX.Element {
 
-    const data: portfolioItemData[] = [
-        {   
-            isExpanded: false,
-            title: "Hello", 
-            thumbnail: "placeholder", 
-            description: "this is a placeholder description"
-        },
-        {   
-            isExpanded: false,
-            title: "Hello 2", 
-            thumbnail: "placeholder", 
-            description: "this is a placeholder description"
-        },
-        {   
-            isExpanded: false,
-            title: "Hello 3", 
-            thumbnail: "placeholder", 
-            description: "this is a placeholder description"
-        },
-        {   
-            isExpanded: false,
-            title: "Hello 4", 
-            thumbnail: "placeholder", 
-            description: "this is a placeholder description"
+    const [expandedItemID, setExpandedItemID] = useState<number>();
+
+    const handleExpand = (itemID: number): void => {
+        //debug
+        console.log("itemid of clicked component: ", itemID);
+        //
+        if (expandedItemID === itemID) {  //if the item is being toggled close, rerender with all portfolioitems closed (only 1 portfolio item can be open at a time)
+            portfolioItems.forEach((item) => {
+                item.props.data.isExpanded = false;
+            });
+            setExpandedItemID(undefined);
+        } else {         //else expand the newly clicked portfolio item and close all the others 
+            portfolioItems.forEach((item) => {
+                if (item.props.itemID === itemID) {
+                    item.props.data.isExpanded = true;
+                } else {
+                    item.props.data.isExpanded = false;
+                }
+            });
+            setExpandedItemID(itemID);
         }
-    ]
+        return;
+    }
+
+    //use effect to update the items when expandeditemid changes 
+/*     useEffect(() => {
+        //if the expandeditemID was not changed to undefined, swap the isExpanded variable on the correct element 
+        if (expandedItemID) {
+            console.log(expandedItemID);
+            const itemToExpand = portfolioItems.find((item) => item.props.itemID == expandedItemID);
+            if (itemToExpand) {
+                itemToExpand.props.data.isExpanded = true;
+                console.log(itemToExpand);
+            } else {
+                console.log("something went wrong in useeffect hook when trying to set the expand boolean.");
+            }
+        }
+    }, [expandedItemID]) */
+
+
+
+    //populate portfolio items
+    //this mock data needs to be replaced with some kind of data retrieval. storing in json data is 
+    //probably a good choice 
+    const portfolioItems: JSX.Element[] = data.map(
+        (item: portfolioItemData, index: number): JSX.Element => {
+            return (
+                <PortfolioItem key={index+1} itemID={index+1} data={item} onExpand={handleExpand} />
+            )
+        }
+    );
+
     return (
         <div className={styles.portfolioMenuContainer}>
-            <PortfolioItem data={data[0]} />
-            <PortfolioItem data={data[1]}/>
-            <PortfolioItem data={data[2]}/>
-            <PortfolioItem data={data[3]}/>
+            {expandedItemID ? (
+                portfolioItems
+            ) : portfolioItems }
         </div>
     );
 }
