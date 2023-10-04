16770b999735c23349dc110a27e91d5a062262e0
gb <glumbongo93@gmail.com>
2023-10-04 19:39:16 +0100

    changed % values to view values in header

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -29,7 +29,8 @@ export default async function Post({ params }: { params: { id: string } }) {
                 <div className={styles.blogContainer}>
                     <div className={styles.blogHeader}>
                         <h1>{post.title}</h1>
-                        <p>{post.date}</p>
+                        <p>Posted: {post.date}</p>
+                        <p>By: Christopher Simmons</p>
                     </div>
                     <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.text}} />
                 </div>
